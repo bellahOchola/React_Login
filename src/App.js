a2c671dbd86b0b8a6ca0499cc140a080ed10d35e
Christabel Ochola <Christabel.Ochola@imbank.co.ke>
2023-12-27 16:10:25 +0300

    Login page

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Login from  './components/login';
+
+function App() {
+  return (
+    <Login></Login>    
+
+  );
+}
+
+export default App;
